9c74659ae2410b3a8303025ec2ab40a692029740
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-11-12 16:58:29 +0100

    Bug fixed, New dropdown color & Todo

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # B207 Lessen
+
 New design for b207.nl with bootstrap 4.
 GitHub Pages isn't working anymore.
 Please clone/download & run with an PHP server.
+
+## Todo
+
+- [ ] More images on the slideshow
+- [ ] Code Examples
+- [ ] Spell check
+- [ ] Remove unused images
+- [ ] Fix dead link
--- public/navigation.php
+++ public/navigation.php
@@ -27,42 +27,20 @@ function displayNav(string $activeMenu)
     {
         echo '<li class="nav-item"><a class="nav-link" href="tools.php">Tools</a></li>';
     }
-    if($activeMenu == "Themes")
-    {
-        echo '
-        <li class="nav-item active dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                Thema&apos;s
-            </a>
-            <div class="dropdown-menu">
-                <a class="dropdown-item" href="themas/accounts.php">Accounts</a>
-                <a class="dropdown-item" href="themas/html_css.php">HTML & CSS</a>
-                <a class="dropdown-item" href="themas/js.php">JavaScript</a>
-                <a class="dropdown-item" href="themas/php_sql.php">PhP & MySQL</a>
-                <a class="dropdown-item" href="themas/modelleren.php">Modelleren</a>
-                <a class="dropdown-item" href="themas/cyber_security.php">Cyber Security</a>
-            </div>
-        </li>';
-    }
-    else
-    {
-        {
-            echo '
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                    Thema&apos;s
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="themas/accounts.php">Accounts</a>
-                    <a class="dropdown-item" href="themas/html_css.php">HTML & CSS</a>
-                    <a class="dropdown-item" href="themas/js.php">JavaScript</a>
-                    <a class="dropdown-item" href="themas/php_sql.php">PhP & MySQL</a>
-                    <a class="dropdown-item" href="themas/modelleren.php">Modelleren</a>
-                    <a class="dropdown-item" href="themas/cyber_security.php">Cyber Sec</a>
-                </div>
-            </li>';
-        }
-    }
+    echo '
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+            Thema&apos;s
+        </a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="themas/accounts.php">Accounts</a>
+            <a class="dropdown-item" href="themas/html_css.php">HTML & CSS</a>
+            <a class="dropdown-item" href="themas/js.php">JavaScript</a>
+            <a class="dropdown-item" href="themas/php_sql.php">PhP & MySQL</a>
+            <a class="dropdown-item" href="themas/modelleren.php">Modelleren</a>
+            <a class="dropdown-item" href="themas/cyber_security.php">Cyber Security</a>
+        </div>
+    </li>';
     if($activeMenu == "Colofon")
     {
         echo '<li class="nav-item active"><a class="nav-link" href="colofon.php">Colofon</a></li>';
